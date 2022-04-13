f34b2832c361e266d26f0f088146f8da40359263
Muhammad Idrees <midexigner@gmail.com>
2022-04-14 02:29:59 +0500

    add more user field

--- config/database.js
+++ config/database.js
@@ -6,6 +6,9 @@ module.exports = ({ env }) => ({
     connection: {
       filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
+    app: {
+      keys: env.array('APP_KEYS') || 129000,
+    },
     useNullAsDefault: true,
   },
 });
