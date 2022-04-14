15d64a5c86d6c0df43b73058d1540acef91397e0
Muhammad Idrees <midexigner@gmail.com>
2022-04-14 17:23:53 +0500

    all schema created

--- config/database.js
+++ config/database.js
@@ -1,10 +1,33 @@
-const path = require('path');
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL);
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
+    connection: {
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
+      ssl: {
+        rejectUnauthorized: false
+      },
+    },
+    app: {
+      keys: env.array('APP_KEYS') || 129000,
+    },
+    debug: false,
+  },
+});
+
+
+/* const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
     client: 'sqlite',
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', 'database/data.db')),
     },
     app: {
       keys: env.array('APP_KEYS') || 129000,
@@ -12,3 +35,4 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+ */
